356f52f22f6d0676bf26c3b0c0cf1231873fe686
Prasad Tajane <tajane.pr@husky.neu.edu>
2017-08-13 15:43:19 -0400

    Modified registerController to add user type

--- public/assignment/views/user/controllers/register.controller.client.js
+++ public/assignment/views/user/controllers/register.controller.client.js
@@ -10,51 +10,76 @@
     function registerController($location, userService)   {
 
         var model = this;
+        model.register  = register;
+        model.createUser= createUser;
 
-        function init() {
+            function init() {
 
         }
         init();
 
-        model.register = (
-            function (user) {
-                //console.log(user);
-                if (user.password1 === user.password2 && typeof user.password1 !== "undefined")   {
 
-                    if (user.username === null || user.username === '' || typeof user.username === 'undefined')   {
-                        alert("Please write an username !")
-                        return;
-                    }
-                    else    {
-                        //model.inuser = userService.findUserByUsername(user.username);
-                        userService.findUserByUsername(user.username)
-                            .then( function (response) {
-                                model.inuser = response.data;
-                                //console.log("model . inuser");
-                                //  console.log(model.inuser);
-                                /*if(model.inuser !== 'null')   {
-                                    alert("Sorry username '" + model.inuser.username + "' already exists !");
-                                }
-                                else */
-                                {
-                                    alert("Welcome " + user.username + " !!!")
-                                    var newUser = {
-                                        username:user.username,
-                                        password:user.password1
-                                    }
-                                    userService.createUser(newUser)
-                                        .then( function (response) {
-                                            newUser = response.data
-                                            alert("Hey, " + user.username + " your userId is " + newUser._id);
-                                            $location.url("/profile/" + newUser._id);
-                                        });
-                                }
-                            });
-                    }
+        function createUser(user) {
+            alert("Welcome " + user.username + " !!!")
+            var newUser = {
+                username:user.username,
+                password:user.password1
+            }
+            userService.createUser(newUser)
+                .then( function (response) {
+                    newUser = response.data
+                    //alert("Hey, " + user.username + " your userId is " + newUser._id);
+                    $location.url("/profile/" + newUser._id);
+                });
+        }
+
+
+        function register(user) {
+                //console.log(user);
+            if (user.type === null || user.type === '' || typeof user.type === 'undefined')   {
+                alert("Please select an 'User Type' !")
+                return;
+            }
+            else if (user.password1 === user.password2 && typeof user.password1 !== "undefined")   {
+
+                if (user.username === null || user.username === '' || typeof user.username === 'undefined')   {
+                    alert("Please write an username !")
+                    return;
                 }
-                else {
-                    alert("Hi " + user.username + ", two passwords do not match!")
+                else    {
+                    createUser(user);
+                    //
+                    /*      CODE TO CHECK IF USER ALREADY EXISTS OR NOT     */
+                    //
+                    //model.inuser = userService.findUserByUsername(user.username);
+                    // userService.findUserByUsername(user.username)
+                    //     .then( function (response) {
+                    //         model.inuser = response.data;
+                    //         console.log("model . inuser");
+                    //          console.log(model.inuser);
+                    //         if(model.inuser !== 'null')   {
+                    //             alert("Sorry username '" + model.inuser.username + "' already exists !");
+                    //         }
+                    //         else
+                    //         {
+                    //             alert("Welcome " + user.username + " !!!")
+                    //             var newUser = {
+                    //                 username:user.username,
+                    //                 password:user.password1
+                    //             }
+                    //             userService.createUser(newUser)
+                    //                 .then( function (response) {
+                    //                     newUser = response.data
+                    //                     //alert("Hey, " + user.username + " your userId is " + newUser._id);
+                    //                     $location.url("/profile/" + newUser._id);
+                    //                 });
+                    //         }
+                    //     });
                 }
-        })
+            }
+            else {
+                alert("Hi " + user.username + ", two passwords do not match!")
+            }
+        }
     }
 })();
